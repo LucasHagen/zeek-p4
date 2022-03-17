c84e00f3e4ecfa5eb058874e6ad08e0eac77de78
Lucas Hagen <lucas_hagen@hotmail.com>
2022-03-17 15:18:45 -0300

    Split docker images
    Signed-off-by: Lucas Hagen <lucas_hagen@hotmail.com>

--- docker/build-all.sh
+++ docker/build-all.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
-for d in ./* ; do
-    [ -d "$d" ] && [ ! -L "$d" ] || continue
+docker build --tag lucashagen/zeek-base ./base
+docker build --tag lucashagen/zeek-full:lts ./full
+docker build --tag lucashagen/zeek-full:rc --build-arg ZEEK_PACKAGE="zeek-rc" --build-arg ZEEK_PACKAGE="/opt/zeek-rc" ./full
+docker build --tag lucashagen/zeek-full --build-arg ZEEK_PACKAGE="zeek-rc" --build-arg ZEEK_PACKAGE="/opt/zeek-rc" ./full
 
-    d_name=$(basename -- "$d")
-    image_name="lucashagen/zeek-$d_name"
+# for d in ./* ; do
+#     [ -d "$d" ] && [ ! -L "$d" ] || continue
 
-    echo "Building '$image_name':"
-    docker build --tag $image_name $d
-done
+#     d_name=$(basename -- "$d")
+#     image_name="lucashagen/zeek-$d_name"
+
+#     echo "Building '$image_name':"
+#     docker build --tag $image_name $d
+# done
 
 echo ""
 echo ""
